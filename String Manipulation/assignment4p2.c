f5f00fafa45379139cd42e10aee36b224e366716
vithumathi <vithu267@hotmail.com>
2017-07-18 12:47:10 -0400

    Add files via upload

---
+++ String Manipulation/assignment4p2.c
@@ -0,0 +1,33 @@
+
+/* Name: Assignment 4 Problem 1
+
+    The purpose of the program is to perform a characterCount & wordcount 
+
+    Author: Vithusanan Mathiaparanam
+    Student Num: 104722017 */
+
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    float characterCount = 0.0f, wordCount = 0.0f; // Input declaration 
+    char ch;
+
+    printf("Enter a sentence: ");
+
+    while ((ch = getchar()) != '\n') {
+
+        if (ch == ' ') {                // Every time a space is encountered, the word count is increased by 1
+            wordCount++;
+            continue;
+        }
+        characterCount++;
+    }
+    
+    wordCount += 1;                     //Since last word isn't counted due to the \n, it is increment by one here.
+    printf("Average word length: %.1f\n\n", characterCount / wordCount);
+
+    return 0;
+}
