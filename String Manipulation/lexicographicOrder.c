8ff917572c2adb0e633993da38e60f00a5fa3f11
vithumathi <vithu267@hotmail.com>
2017-07-18 12:51:17 -0400

    Update lexicographicOrder.c

--- String Manipulation/lexicographicOrder.c
+++ String Manipulation/lexicographicOrder.c
@@ -3,11 +3,12 @@
 
 int main(void){
 
-    char wordList[6][7];
-    int wordCount = 0;
+    char wordList[6][7]; // Stores a list of 6 words up to 6+1 characters each
+    int wordCount = 0;   // How many words is in the list
     int i,j;
     char tempWord[7];
 
+    //Read 4 words
     for (i=0; i<4; i++){
         scanf("%s", wordList[wordCount]);
         wordCount++;
@@ -19,6 +20,7 @@ int main(void){
 
     printf("\n%d\n", wordCount);
 
+    // Sort words using standard bubble sort
     for (i = 1; i < wordCount; i++){
         for (j=0; j < wordCount-1; j++){
             if (strcmp(wordList[j],wordList[j+1])>0){
